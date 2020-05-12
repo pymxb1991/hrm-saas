488d4c52f9ab55d2b48bea860c990741b010ed0c
pymxb1991 <pymxb1991@163.com>
2020-05-13 06:24:26 +0800

    Saas-多租房平台提交     自定义拦截器，no session  问题处理

--- ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
+++ ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
 /**
  * This is Description
@@ -36,5 +37,11 @@ public class SystemApplication {
     @Bean
     public JwtUtils jwtUtils(){
         return  new JwtUtils();
+    }
+
+    //解决no session
+    @Bean
+    public OpenEntityManagerInViewFilter openEntityManagerInViewFilter() {
+        return new OpenEntityManagerInViewFilter();
     }
 }
